ac84bfefa4f8ee459647e592d8a7c95a855369bf
Wes <52631646+wesh92@users.noreply.github.com>
2025-02-12 11:04:20 -0500

    changed models schema in pg

--- systems/postgres/llm_services/models.sql
+++ systems/postgres/llm_services/models.sql
@@ -1,28 +1,29 @@
 create table if not exists public.models
 (
-    id                          text                                                                                        not null
+    id                          text                                                                                             not null
         constraint unique_id
             unique,
-    object                      text          default 'model'::text not null,
-    created timestamp default CURRENT_TIMESTAMP                                                                             not null,
-    owned_by                    text          default 'state_of_tn'::text                                                   not null,
+    object_type                 text          default 'model'::text not null,
+    created_at                  timestamp     default CURRENT_TIMESTAMP                                                          not null,
+    owned_by                    text          default 'state_of_tn'::text                                                        not null,
     local_path                  text,
     base_url_path               text          default 'https://openrouter.ai/api/v1'::text,
-    model_path                  text,
-    available_roles             model_roles[] default ARRAY ['user'::model_roles, 'system'::model_roles, 'ai'::model_roles] not null,
+    model_path                  text generated always as (((lower(owned_by) || '/'::text) ||
+                                                           lower(regexp_replace(id, '\s+'::text, '-'::text, 'g'::text)))) stored not null,
+    available_roles             model_roles[] default ARRAY ['user'::model_roles, 'system'::model_roles, 'ai'::model_roles]      not null,
     max_input_token_window_size bigint,
     max_output_token_size       bigint,
     constraint models_pk
-        primary key (id, object, created, owned_by)
+        primary key (id, object_type, owned_by, model_path)
 );
 
 comment on table public.models is 'Metadata About Models';
 
 comment on column public.models.id is 'Human-friendly Model Name (eg. "R1 Distill Llama 70b")';
 
-comment on column public.models.object is 'model type';
+comment on column public.models.object_type is 'model type';
 
-comment on column public.models.created is 'When the model or other type was created';
+comment on column public.models.created_at is 'When the model or other type was created';
 
 comment on column public.models.owned_by is 'Owning User or Org';
 
@@ -38,3 +39,4 @@ comment on column public.models.max_output_token_size is 'Maximum expected outpu
 
 alter table public.models
     owner to postgres;
+
